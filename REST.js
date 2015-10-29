a2589bb11c80716dbe779b0b523d08c4c6aede17
Eduardo Chaves <educostachaves@hotmail.com>
2015-10-29 15:35:16 -0200

    finishing simple Restful Node and MySQL

--- REST.js
+++ REST.js
@@ -11,6 +11,7 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
         res.json({"Message" : "Hello World !"});
     });
 
+    //Get All Users
     router.get("/users",function(req,res){
         var query = "SELECT * FROM ??";
         var table = ["user_login"];
@@ -24,6 +25,7 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
         });
     });
 
+    //Get User By ID
     router.get("/users/:user_id",function(req,res){
         var query = "SELECT * FROM ?? WHERE ??=?";
         var table = ["user_login","user_id",req.params.user_id];
@@ -37,6 +39,7 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
         });
     });
 
+    //Insert New User
     router.post("/users",function(req,res){
         var query = "INSERT INTO ??(??,??) VALUES (?,?)";
         var table = ["user_login","user_email","user_password",req.body.email,md5(req.body.password)];
@@ -50,6 +53,7 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
         });
     });
 
+    //Update an User
     router.put("/users",function(req,res){
         var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
         var table = ["user_login","user_password",md5(req.body.password),"user_email",req.body.email];
@@ -63,6 +67,7 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
         });
     });
 
+    //Delete a User By Email
     router.delete("/users/:email",function(req,res){
         var query = "DELETE from ?? WHERE ??=?";
         var table = ["user_login","user_email",req.params.email];
@@ -75,6 +80,119 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
             }
         });
     });
+
+    //Get UserStatus by ID
+    router.get("/users_status/:id_user",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ?? = ?";
+        var table = ["user_status","user_id_fk",req.params.id_user];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users_Status" : rows});
+            }
+        });
+    });
+
+    //Insert New Status
+    router.post("/users_status/:id_user",function(req,res){
+        var query = "INSERT INTO ??(??,??) VALUES (?,?)";
+        var table = ["user_status","user_id_fk","status_text",req.params.id_user,req.body.status_text];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Status Added !"});
+            }
+        });
+    });
+
+    //Update Status
+    router.put("/users_status/:id_user",function(req,res){
+        var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?)";
+        var table = ["user_status","status_text",req.body.status_text,"user_id_fk",req.params.id_user,];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Status Updated!"});
+            }
+        });
+    });
+
+    //Delete an UserStatus By id_user
+    router.delete("/users_status/:id_user",function(req,res){
+        var query = "DELETE from ?? WHERE ??=?";
+        var table = ["user_status","user_id_fk",req.params.id_user];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Deleted the user with ID "+req.params.id_user});
+            }
+        });
+    });
+
+    //Get UserInfo by ID
+    router.get("/users_info/:id_user",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ?? = ?";
+        var table = ["user_info","user_id_fk",req.params.id_user];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users_Info" : rows});
+            }
+        });
+    });
+
+    //Insert New Info
+    router.post("/users_info/:id_user",function(req,res){
+        var query = "INSERT INTO ??(??,??,??) VALUES (?,?,?)";
+        var table = ["user_info","user_id_fk","user_name","user_location",req.params.id_user,req.body.name,req.body.location,];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Info Added !"});
+            }
+        });
+    });
+
+    //Update Status
+    router.put("/users_info/:id_user",function(req,res){
+        var query = "UPDATE ?? SET ?? = ?, ?? = ? WHERE ?? = ?)";
+        var table = ["user_info","user_name",req.body.name,"user_location",req.body.location,"user_id_fk",req.params.id_user,];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Status Updated!"});
+            }
+        });
+    });
+
+    //Delete an UserStatus By id_user
+    router.delete("/users_info/:id_user",function(req,res){
+        var query = "DELETE from ?? WHERE ??=?";
+        var table = ["user_info","user_id_fk",req.params.id_user];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Deleted the user with ID "+req.params.id_user});
+            }
+        });
+    });
+
 }
 
 module.exports = REST_ROUTER;
